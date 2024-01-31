a9784745ce5031dc641e004ff267c54b475234b2
Siddharth Awadh <96815992+awadhsiddharth@users.noreply.github.com>
2024-01-31 06:00:55 +0000

    spring done

---
+++ Security/Ss2022C1E1Application.java
@@ -0,0 +1,13 @@
+// package com.acc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ss2022C1E1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ss2022C1E1Application.class, args);
+	}
+
+}
